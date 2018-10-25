4edce97141a791807488daaf6c58d12908905090
Shane D Wilson <me@shanedwilson.com>
2018-10-25 18:40:14 -0500

    working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,11 @@
 import locationsComponent from "./components/locationsComponent.js"
 import movieComponent from "./components/movieComponent.js"
+// import events from "./helpers/events.js"
 
 const loadApp = () => {
     locationsComponent.initialLocationsView();
     movieComponent.initializeMovieView();
+    $('#Back').hide();
 }
 
 loadApp();
